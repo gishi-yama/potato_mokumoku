52589c9deb95f8453d46a47f4f9182a5b933b87b
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2020-04-01 20:27:41 +0900

    Define a Bicycle constructor まで動作確認

---
+++ dart/src/from-java-to-dart/define_a_bicycle_constructor.dart
@@ -0,0 +1,18 @@
+class Bicycle {
+  int cadence;
+  int speed;
+  int gear;
+
+  // コンストラクタのケツにはセミコロンが必要
+  // thisとここで書くと、インスタンスフィールドのことを指す
+  Bicycle(this.cadence, this.speed, this.gear); 
+
+}
+
+void main() {
+  var hoge = new Bicycle(10, 20, 30);
+  print(hoge.cadence);
+  print(hoge.speed);
+  print(hoge.gear);
+}
+
